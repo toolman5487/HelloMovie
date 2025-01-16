e3a387328a331404505caa3dd3c37548d20abf1c
Willy <willy548798@gmail.com>
2025-01-16 17:19:38 +0800

    Initial Commit

---
+++ HelloMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloMovie
+//
+//  Created by Willy Hsu on 2025/1/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
